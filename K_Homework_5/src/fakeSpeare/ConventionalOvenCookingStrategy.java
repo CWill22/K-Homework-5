c92455761dfed59dffba24bfe6d7234fecec80f0
Braeden <158224803+oRackzyy@users.noreply.github.com>
2024-03-10 20:43:21 -0500

    Added setCookingPrice

--- K_Homework_5/src/fakeSpeare/ConventionalOvenCookingStrategy.java
+++ K_Homework_5/src/fakeSpeare/ConventionalOvenCookingStrategy.java
@@ -4,8 +4,8 @@ public class ConventionalOvenCookingStrategy implements ICookingStrategy {
 
 	@Override
 	public boolean cook(AbstractPizza pizza) {
-		// TODO Auto-generated method stub
-		return false;
+		pizza.setCookingPrice(pizza.getCookingPrice() + CookingStyleType.CONVENTIONAL_OVEN.getAdditionalCost());
+		return true;
 	}
 
 }
