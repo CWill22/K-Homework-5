9e4c53c3c04f2e06cf3cb43fa4384abdc94b6d1f
CWill22 <147526756+CWill22@users.noreply.github.com>
2024-03-11 14:40:19 -0500

    add comments to CookingStyleType enum

--- K_Homework_5/src/fakeSpeare/CookingStyleType.java
+++ K_Homework_5/src/fakeSpeare/CookingStyleType.java
@@ -1,12 +1,14 @@
 package fakeSpeare;
 
 public enum CookingStyleType {
+	// This is the enum for the cooking styles.
 	MICROWAVE,
 	CONVENTIONAL_OVEN,
 	BRICK_OVEN;
 	
+	// This is the default constructor for the enum.
 	CookingStyleType() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
