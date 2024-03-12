5d55a50575a59ecc5ccd3f7d5870209cd36a8b2f
Ethan Darling <158128266+edar115@users.noreply.github.com>
2024-03-12 14:59:16 -0500

    Update MicrowaveCookingStrategy.java

--- K_Homework_5/src/fakeSpeare/MicrowaveCookingStrategy.java
+++ K_Homework_5/src/fakeSpeare/MicrowaveCookingStrategy.java
@@ -3,6 +3,7 @@ package fakeSpeare;
 public class MicrowaveCookingStrategy implements ICookingStrategy {
 
 	// This class is a concrete strategy for cooking a pizza in a microwave.
+	// It increases the cooking price of the pizza by 1.0 and sets the cooking strategy to this MicrowaveCookingStrategy
 	@Override
 	public boolean cook(AbstractPizza pizza) {
 		pizza.setCookingPrice(pizza.getCookingPrice() + 1.00);
